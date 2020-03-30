a34e5ba8417edbd41596d34c8392dd511fa889c9
weinandai <1334815935@qq.com>
2020-03-30 18:35:12 +0800

    程序说明完成

--- ruan_zhu/ruan_zhu_blade_ansys/main.py
+++ ruan_zhu/ruan_zhu_blade_ansys/main.py
@@ -10,8 +10,8 @@ import geatpy as ea
 # 初始化配置文件
 opt = CONFIG(project_path='F:/ansys_program/windblade/ansys_model/',
              csv_path='F:/ansys_program/windblade/useful_csv/',
-             Encoding='BG', NIND=20,
-             MAXGEN=300, Drawing=1, plot_use=False,
+             Encoding='BG', NIND=20,MAXGEN=300,
+             Drawing=1, plot_use=False,
              start_time=time.time(), ansys_time=1, primary_index=1)
 
 # ipdb.set_trace()
@@ -26,6 +26,5 @@ model.model()
 ansys_nsga2 = NSGA2_ANSYS(myproblem=MyProblem(model), Encoding=opt.Encoding,
                           NIND=opt.NIND, MAXGEN=opt.MAXGEN, Drawing=opt.Drawing)
 ansys_nsga2.run_nsga()
-
 # 关闭ansys后台
 ansys.kill()
